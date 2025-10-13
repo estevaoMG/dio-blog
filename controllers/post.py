641e6aec9f8c743885e09164d1ff29a281b192c4
estevaoMG <estevao.gouveia@hotmail.com>
2025-10-12 21:51:27 -0300

    Retornando dadosem formato JSON

---
+++ controllers/post.py
@@ -0,0 +1,69 @@
+from datetime import UTC, datetime
+from typing import Annotated
+
+from fastapi import Cookie, FastAPI, Header, Response, status
+
+from schemas.post import PostIn
+from views.post import PostOut
+
+fake_db = [
+    {
+        "title": f"Criando uma aplicação com Django",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": f"Criando uma aplicação com FastAPI",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": f"Criando uma aplicação com Flask",
+        "date": datetime.now(UTC),
+        "published": True,
+    },
+    {
+        "title": f"Criando uma aplicação com Starlett",
+        "date": datetime.now(UTC),
+        "published": False,
+    },
+]
+
+
+@app.post("/posts/", status_code=status.HTTP_201_CREATED, response_model=PostOut)
+def create_post(post: PostIn):
+    fake_db.append(post.model_dump())
+    return post
+
+
+@app.get("/posts/", response_model=list[PostOut])
+def read_posts(
+    response: Response,
+    published: bool,
+    limit: int,
+    skip: int = 0,
+    ads_id: Annotated[str | None, Cookie()] = None,
+    user_agent: Annotated[str | None, Header()] = None,
+):
+    response.set_cookie(key="user", value="estevao.gouveia@hotmail.com")
+    print(f"Cookie: {ads_id}")
+    print(f"User-agent: {user_agent}")
+    return [
+        post for post in fake_db[skip : skip + limit] if post["published"] is published
+    ]
+
+
+@app.get("/posts/{framework}", response_model=PostOut)
+def read_framework_posts(framework: str):
+    return {
+        "posts": [
+            {
+                "title": f"Criando uma aplicação com {framework}",
+                "date": datetime.now(UTC),
+            },
+            {
+                "title": f"Internacionalizando uma app {framework}",
+                "date": datetime.now(UTC),
+            },
+        ]
+    }
