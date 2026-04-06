5808c2b3555bf5aedd5e29901b137e593ad59129
estevaoMG <estevao.gouveia@hotmail.com>
2026-04-06 18:48:43 -0300

    Conexão a banco de dados assíncrono

--- main.py
+++ main.py
@@ -1,6 +1,23 @@
+from contextlib import asynccontextmanager
+import sqlalchemy as sa
+import databases
 from fastapi import FastAPI
-
 from controllers import post
 
-app = FastAPI()
+DATABASE_URL = "sqlite:///./blog.db"
+
+database = databases.Database(DATABASE_URL)
+metadata = sa.MetaData()
+engine = sa.create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+metadata.create_all(engine)
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    await database.connect()
+    yield
+    await database.disconnect()
+
+
+app = FastAPI(lifaspan=lifespan)
 app.include_router(post.router)
---
+++ models/post.py
@@ -0,0 +1,12 @@
+import sqlalchemy as sa
+from main import metadata
+
+posts = sa.Table(
+    "posts",
+    metadata,
+    sa.Column("Id", sa.Integer, primary_key=True),
+    sa.Column("title", sa.String[150], nullable=False, unique=True),
+    sa.Collumn("content", sa.String, nullable=False),
+    sa.Column("published_at", sa.DateTime, nullable=True),
+    sa.Column("publiched", sa.Boolean, default=False),
+)
